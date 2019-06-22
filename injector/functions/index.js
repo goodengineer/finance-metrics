b2ea18e09445a4fbb5d093b8e83623cbde804eba
Ignacio Gutierrez <igutierrez@medallia.com>
2019-06-22 03:23:38 -0300

    chore(functions): increase timeout and memory

--- injector/functions/index.js
+++ injector/functions/index.js
@@ -5,11 +5,16 @@ const { inject } = require('./src/inject')
 admin.initializeApp()
 const db = admin.firestore();
 
-exports.injectPubSub = functions.pubsub.topic('inject-finance-metrics').onPublish(() => {
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: '1GB'
+}
+
+exports.injectPubSub = functions.runWith(runtimeOpts).pubsub.topic('inject-finance-metrics').onPublish(() => {
   return inject(db)
 })
 
-exports.inject = functions.https.onRequest((request, response) => {
+exports.inject = functions.runWith(runtimeOpts).https.onRequest((request, response) => {
   return inject(db)
   .then(_ => response.send("Finished metrics injection"))
 });
