e5e068236a00691acecb9531df153d28b3584c12
Ignacio Gutierrez <pepo.dev@gmail.com>
2019-07-14 13:35:41 -0300

    update inject

--- injector/functions/index.js
+++ injector/functions/index.js
@@ -1,20 +1,7 @@
 const admin = require('firebase-admin');
-const functions = require('firebase-functions');
 const { inject } = require('./src/inject')
 
 admin.initializeApp()
 const db = admin.firestore();
 
-const runtimeOpts = {
-  timeoutSeconds: 300,
-  memory: '1GB'
-}
-
-exports.injectPubSub = functions.runWith(runtimeOpts).pubsub.topic('inject-finance-metrics').onPublish(() => {
-  return inject(db)
-})
-
-exports.inject = functions.runWith(runtimeOpts).https.onRequest((request, response) => {
-  return inject(db)
-  .then(_ => response.send("Finished metrics injection"))
-});
+inject(db)
